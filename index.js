8ace1fda418dae54d7ceec01766cf8c2c224c775
kygrove12 <kygrove12@gmail.com>
2018-08-11 09:12:08 -0700

    Added portfolio link

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const openWeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?appid=94
 
 
 function hikesTemplate(hike, image){
-	console.log ("hikesTemplate ran");
 	$('.hikesSection').append(`
 		<a href=${hike.url} target="_blank">
 			<img class="trailImage" src=${image} alt="trail image" onerror="this.src='https://images.unsplash.com/uploads/1412533519888a485b488/bb9f9777';">
@@ -20,7 +19,6 @@ function hikesTemplate(hike, image){
 }
 
 function displayHikes(data){
-	console.log (data);
 	$('.hikesSection').html("");
 	let trailsFound = data.trails;
 	let trailImageArr = ['https://images.unsplash.com/uploads/1412533519888a485b488/bb9f9777','https://images.unsplash.com/photo-1436918671239-a2b72ace4880','https://images.unsplash.com/photo-1420802498636-9d647b43d2eb','https://images.unsplash.com/photo-1444583791700-0bd8d26df657','https://images.unsplash.com/uploads/1411949218166d2b71460/5f516109','https://images.unsplash.com/photo-1458170143129-546a3530d995','https://images.unsplash.com/photo-1476979735039-2fdea9e9e407','https://images.unsplash.com/photo-1519309621146-2a47d1f7103a','https://images.unsplash.com/photo-1525474089639-b5fff4440315','https://images.unsplash.com/photo-1526078192901-a5f41bf14eab'];
@@ -50,7 +48,6 @@ function getHikes (lat, lon){
 		};
 	$.ajax(settings)
 	.fail(function(){
-		console.log ("getHikes failed");
 		$('.hikesSection').html(`<h2 class="hikesError">No hikes found. Please try a different search.</h2>`);
 	});
 }
@@ -76,13 +73,11 @@ function weatherTemplate (data) {
 }
 
 function weatherDisplay(data){
-	console.log (data);
 	getHikes (data.coord.lat, data.coord.lon);
 	$(".weatherSection").html(weatherTemplate(data));
 }
 
 function getLocalWeather(){
-	console.log("getLocalWeather ran");
 	let place = $("#location").val();
 
 	const settings = {
